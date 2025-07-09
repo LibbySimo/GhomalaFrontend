97cf9d9db7847e899737e8183a61a593e0d241c8
LIBBY MINGA SIMO <155213968+LibbySimo@users.noreply.github.com>
2025-07-09 20:30:53 +0100

    Update useDocumentQuery.js

--- src/hooks/useDocumentQuery.js
+++ src/hooks/useDocumentQuery.js
@@ -1,7 +1,7 @@
 import { useState } from 'react';
 import axios from 'axios';
 
-const API_URL =  'https://ghomalabackendv2-production.up.railway.app/api';
+const API_URL =  'https://ghomalaapi.onrender.com/api';
 
 export const useDocumentQuery = () => {
   const [isLoading, setIsLoading] = useState(false);
